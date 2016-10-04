cf31564407499791a28f0f9c26d1b51f20eaadce
james.powis <james.powis@viawest.com>
2016-10-04 17:28:16 -0600

    Added git support
    Also added ability to exclude specific modules from version checking

--- lib/ra10ke.rb
+++ lib/ra10ke.rb
@@ -1,6 +1,7 @@
 require 'rake'
 require 'rake/tasklib'
 require 'ra10ke/version'
+require 'git'
 
 module Ra10ke
   class RakeTask < ::Rake::TaskLib
@@ -15,7 +16,14 @@ module Ra10ke
           puppetfile = R10K::Puppetfile.new('.')
           puppetfile.load!
 
+          # ignore file allows for "don't tell me about this"
+          ignore_modules = []
+          if File.exist?('.r10kignore')
+            ignore_modules = File.readlines('.r10kignore').each {|l| l.chomp!}
+          end
+
           puppetfile.modules.each do |puppet_module|
+            next if ignore_modules.include? puppet_module.title
             if puppet_module.class == R10K::Module::Forge
               module_name = puppet_module.title.gsub('/', '-')
               forge_version = PuppetForge::Module.find(module_name).current_release.version
@@ -23,6 +31,39 @@ module Ra10ke
               if installed_version != forge_version
                 puts "#{puppet_module.title} is OUTDATED: #{installed_version} vs #{forge_version}"
               end
+            end
+
+            if puppet_module.class == R10K::Module::Git
+              remote = puppet_module.instance_variable_get(:@remote)
+              ref    = puppet_module.instance_variable_get(:@desired_ref)
+
+              # some guards to prevent unnessicary execution
+              next unless ref
+              next if ref == 'master'
+
+              remote_refs = Git.ls_remote(remote)
+
+              # skip if ref is a branch
+              next if remote_refs['branches'].key?(ref)
+
+              ref_type = 'sha'    if ref.match(/^[a-z0-9]{40}$/)
+              ref_type = 'tag'    if remote_refs['tags'].key?(ref)
+              case ref_type
+              when 'sha'
+                latest_ref = remote_refs['head'][:sha]
+              when 'tag'
+                # we have to be opinionated here, due to multiple conventions only one can reign suppreme
+                # as such tag v#.#.# is what will pick.
+                if ref.match(/^[vV]\d/)
+                  tags = remote_refs['tags']
+                  version_tags = tags.select { |f| /^[vV]\d/.match(f) }
+                  latest_ref = version_tags.keys.sort.last
+                end
+              else
+                raise "Unable to determine ref_type for #{puppet_module.title}"
+              end
+
+              puts "#{puppet_module.title} is OUTDATED: #{ref} vs #{latest_ref}" if ref != latest_ref
             end
           end
         end
