e9977cb2d3cf56a1ca19459729676d88bb21814f
Corey Osman <corey@nwops.io>
2022-04-21 17:18:04 -0500

    Refactors dependencies code to use table format for output
      * Previously it was difficult to read the output of the task
        This now outputs into a table
    
      * A refactor was needed to make this into a class for proper testing
      * As a side benefit it is now easier to output the content into
        future additional formats such as json, puppetfile and yaml if needed.

--- spec/ra10ke/dependencies_spec.rb
+++ spec/ra10ke/dependencies_spec.rb
@@ -1,34 +1,56 @@
 # frozen_string_literal: true
-
+require 'r10k/puppetfile'
 require 'spec_helper'
 require 'ra10ke/dependencies'
 RSpec::Mocks.configuration.allow_message_expectations_on_nil = true
 
 RSpec.describe 'Ra10ke::Dependencies::Verification' do
-  after(:each) do
-    # reset version formats
-    formats = Ra10ke::Dependencies.class_variable_get(:@@version_formats)
-    Ra10ke::Dependencies.class_variable_set(:@@version_formats, formats.select { |k, _v| k == :semver } )
+  let(:instance) do
+    pfile = R10K::Puppetfile.new(File.basename(puppetfile), nil,puppetfile, nil, false)
+    Ra10ke::Dependencies::Verification.new(pfile)
+  end
+
+  let(:puppetfile) do
+    File.join(fixtures_dir, 'Puppetfile')
   end
 
   context 'register_version_format' do
     it 'default contains semver' do
-      expect(Ra10ke::Dependencies.class_variable_get(:@@version_formats)).to have_key(:semver)
+      expect(Ra10ke::Dependencies::Verification.version_formats).to have_key(:semver)
     end
     it 'add new version format' do
-      Ra10ke::Dependencies.register_version_format(:test) do |tags|
+      Ra10ke::Dependencies::Verification.register_version_format(:test) do |tags|
         nil
       end
-      expect(Ra10ke::Dependencies.class_variable_get(:@@version_formats)).to have_key(:test)
+      expect(Ra10ke::Dependencies::Verification.version_formats).to have_key(:test)
     end
   end
 
-  context 'get_latest_ref' do
+  context 'show output in table format' do
     let(:instance) do
-      class DependencyDummy
-        include Ra10ke::Dependencies
-      end.new
+      pfile = R10K::Puppetfile.new(File.basename(puppetfile), nil,puppetfile, nil, false)
+      Ra10ke::Dependencies::Verification.new(pfile)
     end
+  
+    let(:puppetfile) do
+      File.join(fixtures_dir, 'Puppetfile')
+    end
+
+    let(:processed_modules) do
+      instance.outdated
+    end
+
+    it 'have dependencies array' do
+      expect(processed_modules).to be_a Array
+    end
+
+    it 'show dependencies as table' do
+      instance.print_table(processed_modules)
+    end
+
+  end
+
+  context 'get_latest_ref' do
 
     context 'find latest semver tag' do
       let(:latest_tag) do
@@ -60,13 +82,13 @@ RSpec.describe 'Ra10ke::Dependencies::Verification' do
       end
 
       it do
-        Ra10ke::Dependencies.register_version_format(:number) do |tags|
+        Ra10ke::Dependencies::Verification.register_version_format(:number) do |tags|
           tags.detect { |tag| tag == latest_tag }
         end
         expect(instance.get_latest_ref({
           'tags' => test_tags,
         })).to eq(latest_tag)
       end
-    end
+     end
   end
 end
